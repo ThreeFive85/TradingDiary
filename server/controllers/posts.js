d5eaec977b78cd193a07f81fd6a89a77978dc3c9
박동혁 <kaje0god@naver.com>
2021-04-01 15:47:24 +0900

    Feat : controllers folder

---
+++ server/controllers/posts.js
@@ -0,0 +1,19 @@
+import mysql from 'mysql2/promise';
+import { db } from '../config/db.js';
+
+const pool = mysql.createPool(db);
+
+export const getDiary = async(req, res) => { // async, await
+
+    const connection = await pool.getConnection();
+
+    try {
+        let result = await connection.query("SELECT * FROM diary")
+        if (result[0].length < 1) {
+            throw new Error('nothing')
+        }
+        res.status(202).json(result[0])
+    } catch (err) {
+        console.log(err)
+    }
+}
