2cc4bfdde18765baff11fe3fb5284c0eaf827300
박동혁 <kaje0god@naver.com>
2021-04-07 15:26:09 +0900

    Fix : router  - currentStock router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import { getDiary, createDiary } from '../controllers/posts.js';
+import { getCurrentStock, createCurrentStock } from '../controllers/current.js';
 
 const router = express.Router();
 
 router.get('/', getDiary);
 router.post('/', createDiary);
+router.get('/', getCurrentStock);
+router.post('/', createCurrentStock);
 
 export default router;
