0af7e7f376c8a6c0cd07f89e8e0df5077dc21844
박동혁 <kaje0god@naver.com>
2021-04-16 09:33:32 +0900

    Fix : HeaderBar component

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import useStyles from './styles';
 
 const HeaderBar = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            제목부분
-        </div>
+        <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            매매일지
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
     )
 }
 
