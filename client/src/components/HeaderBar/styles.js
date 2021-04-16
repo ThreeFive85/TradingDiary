6290714b6cbb085d66ffafc77a66a1e198c85481
박동혁 <kaje0god@naver.com>
2021-04-16 09:34:14 +0900

    Fix : HeaderBar style

---
+++ client/src/components/HeaderBar/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+      },
+      menuButton: {
+        marginRight: theme.spacing(2),
+      },
+  }));
+  
