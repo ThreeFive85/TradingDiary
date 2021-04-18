df6802eaaaf1e03dd77ba184f248e8e211313446
박동혁 <kaje0god@naver.com>
2021-04-18 20:33:58 +0900

    Fix : Complete component

--- client/src/components/Complete/Complete.js
+++ client/src/components/Complete/Complete.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const Complete = () => {
+import useStyles from './styles';
+
+function createData(name, calories, fat, carbs, protein, day1, day2) {
+    return { name, calories, fat, carbs, protein, day1, day2 };
+  }
+  
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0, '20-10-11', '21-10-11'),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3, '20-10-11', '21-10-11'),
+    createData('Eclair', 262, 16.0, 24, 6.0, '20-10-11', '21-10-11'),
+    createData('Cupcake', 305, 3.7, 67, 4.3, '20-10-11', '21-10-11'),
+    createData('Gingerbread', 356, 16.0, 49, 3.9, '20-10-11', '21-10-11'),
+    createData('Gingerbread', 356, 16.0, 49, 3.9, '20-10-11', '21-10-11'),
+    createData('Gingerbread', 356, 16.0, 49, 3.9, '20-10-11', '21-10-11'),
+  ];
+
+
+const Holding = () => {
+    const classes = useStyles();
+    
     return (
-        <div>
-            완료한 부분
-        </div>
+        <TableContainer component={Paper}>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>정산 종목</Paper>
+        </Grid>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>종목</TableCell>
+              <TableCell align="right">총매수금액&nbsp;(원)</TableCell>
+              <TableCell align="right">총매도금액&nbsp;(원)</TableCell>
+              <TableCell align="right">처음거래일</TableCell>
+              <TableCell align="right">마지막거래일</TableCell>
+              <TableCell align="right">수익률&nbsp;(%)</TableCell>
+              <TableCell align="right">수익금&nbsp;(원)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.day1}</TableCell>
+                <TableCell align="right">{row.day2}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
 }
 
-export default Complete
+export default Holding
