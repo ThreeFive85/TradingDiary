73bc64931cf3c3dd42b9c8419a831b7a9a8a33cf
박동혁 <kaje0god@naver.com>
2021-04-18 20:34:32 +0900

    Fix : Complete component style

---
+++ client/src/components/Complete/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    table: {
+        minWidth: 650,
+      },
+      paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      },
+  }));
+  
