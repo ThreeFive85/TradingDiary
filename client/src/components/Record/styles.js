0650cd61d6ce73c8f2965b537618db4a4d4b658d
박동혁 <kaje0god@naver.com>
2021-04-20 10:13:06 +0900

    Fix : Record title style

--- client/src/components/Record/styles.js
+++ client/src/components/Record/styles.js
@@ -1,19 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      height: 100,
-      width: '100%',
-      '& .MuiFormGroup-options': {
-        alignItems: 'center',
-        paddingBottom: theme.spacing(1),
-        '& > div': {
-          minWidth: 100,
-          margin: theme.spacing(2, 2, 2, 0),
-        },
-      },
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
     },
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
   }));
   
