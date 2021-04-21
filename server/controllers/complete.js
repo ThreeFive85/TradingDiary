a4980d9bae6dc22803b15b77a8b64b62c63d4633
박동혁 <kaje0god@naver.com>
2021-04-21 20:06:33 +0900

    Fix : add finally

--- server/controllers/complete.js
+++ server/controllers/complete.js
@@ -10,10 +10,13 @@ export const completeStock = async(req, res) => { // async, await
     try {
         let result = await connection.query("SELECT * FROM completeStock")
         if (result[0].length < 1) {
-            throw new Error('nothing')
+            // throw new Error('nothing')
+            console.log('nothing')
         }
         res.status(202).json(result[0])
     } catch (err) {
         console.log(err)
+    } finally {
+        connection.release()
     }
 }
