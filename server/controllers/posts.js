1400401d3626113187234d60a6e53673d5d5174a
박동혁 <kaje0god@naver.com>
2021-04-24 20:55:20 +0900

    Fix : query

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,14 +6,14 @@ const pool = mysql.createPool(db);
 export const getDiary = async(req, res) => { // async, await
 
     const connection = await pool.getConnection();
-    let query = 'SELECT 종목명, 종목형태, 매매형태, 매매단가, '+
+    let query = 'SELECT ID, 종목명, 종목형태, 매매형태, 매매단가, '+
     `replace(매매수량, '.00000000', '') AS 매매수량, `+
     '매매금액, 매매일자 FROM diary'
 
     try {
         let result = await connection.query(query)
         if (result[0].length < 1) {
-            throw new Error('nothing')
+            console.log("nothing")
         }
         res.status(202).json(result[0])
     } catch (err) {
@@ -27,15 +27,19 @@ export const createDiary = async(req, res) => { // async, await
 
     const {종목명, 종목형태, 매매형태, 매매단가, 매매수량, 매매금액, 매매일자} = req.body;
 
-    console.log(req.body)
+    // console.log(req.body)
 
     const connection = await pool.getConnection();
 
     try {
         const data = await connection.query('insert into diary set ?', { 종목명: 종목명, 종목형태: 종목형태,
         매매형태: 매매형태, 매매단가: 매매단가, 매매수량: 매매수량, 매매금액: 매매금액, 매매일자: 매매일자 })
-        
-        res.status(202).json(data[0])
+        // console.log("data : ", data)
+        let query = 'SELECT ID, 종목명, 종목형태, 매매형태, 매매단가, '+
+                `replace(매매수량, '.00000000', '') AS 매매수량, `+
+                '매매금액, 매매일자 FROM diary WHERE ID = ?'
+        const result = await connection.query(query, [data[0].insertId])
+        res.status(202).json(result[0][0])
     } catch (err) {
         console.log(err)
     } finally {
