b1fc3fe4b145f306ae13c02d643475e4c9296330
박동혁 <kaje0god@naver.com>
2021-04-26 15:53:35 +0900

    Fix : combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
 import posts from './posts';
+import current from './current';
+import complete from './complete';
 
-export default combineReducers({ posts });
+export default combineReducers({ posts, current, complete });
