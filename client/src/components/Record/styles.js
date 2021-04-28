29e646d80f281eb9f9f1185b910d6623a82399db
박동혁 <kaje0god@naver.com>
2021-04-28 15:09:42 +0900

    Fix : Record component styles

--- client/src/components/Record/styles.js
+++ client/src/components/Record/styles.js
@@ -2,15 +2,18 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   root: {
+    marginBottom: 30,
     '& > *': {
       margin: theme.spacing(1),
       width: '25ch',
     },
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  button: {
+    margin: theme.spacing(1),
   },
   }));
   
