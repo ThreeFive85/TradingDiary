6d7665d43286e93f6f38babf223827e18d2a4151
박동혁 <kaje0god@naver.com>
2021-05-01 11:41:52 +0900

    Fix : HeaderBar component styles

--- client/src/components/HeaderBar/styles.js
+++ client/src/components/HeaderBar/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        padding: 35
       },
       menuButton: {
         marginRight: theme.spacing(2),
