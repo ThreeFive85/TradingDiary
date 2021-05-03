d76357a4646b8592f0b561837131d014cb41da87
박동혁 <kaje0god@naver.com>
2021-05-03 11:49:47 +0900

    Fix : add urls

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 const url = 'http://localhost:4000/diary';
+const url2 = 'http://localhost:4000/current';
+const url3 = 'http://localhost:4000/complete';
 
 export const fetchPosts = () => axios.get(url);
 export const createDiary = (newPost) => axios.post(url, newPost);
+export const fetchCurrents = () => axios.get(url2);
+export const fetchComplete = () => axios.get(url3);
