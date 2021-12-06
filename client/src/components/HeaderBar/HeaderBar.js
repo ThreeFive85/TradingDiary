789d450f7e1a877084e1645a5d5c03f4b3240bd8
박동혁 <kaje0god@naver.com>
2021-12-06 09:30:35 +0900

    Fix : headerBar

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import {Toolbar, Button} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+// import Home from './components/Home/Home.js';
+
+import {getCurrent} from '../../actions/current';
 
 import useStyles from './styles';
 
 const HeaderBar = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
 
     return (
-        <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            매매일지
-          </Typography>
+      <AppBar className={classes.root} position="static">
+        <Toolbar className={classes.toolbar}>
+          <div className={classes.buttons}>
+          <Button variant="contained" color="secondary">
+            main
+          </Button>
+          <Button variant="contained" color="secondary">
+            history
+          </Button>
+          <Button variant="contained" color="secondary">
+            rate
+          </Button>
+          </div>
         </Toolbar>
       </AppBar>
-    </div>
     )
 }
 
