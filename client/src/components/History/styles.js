8015065c9070443e815e0708a9b23f44988afbb0
박동혁 <kaje0god@naver.com>
2021-12-19 10:44:42 +0900

    Fix: History component styles

--- client/src/components/History/styles.js
+++ client/src/components/History/styles.js
@@ -1,11 +1,41 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 
-export default makeStyles((theme) => ({
-    root: {
-      width: '100%',
-      marginBottom: 30,
+export const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+export const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
     },
-    container: {
-      maxHeight: 440,
-    },
-  }));
+  },
+}))(TableRow);
+
+export const useStyles = makeStyles((theme)=>({
+  table: {
+    minWidth: 700,
+  },
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+    marginBottom: 10
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
