1d9d10cf5961a947690a94b922c25f4e5e0058e1
박동혁 <kaje0god@naver.com>
2021-12-23 12:10:33 +0900

    Fix: posts server controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -23,6 +23,29 @@ export const getDiary = async(req, res) => { // async, await
     }
 }
 
+export const getStock = async(req, res) => { // async, await
+
+    const {name} = req.params;
+    // console.log(name)
+
+    const connection = await pool.getConnection();
+    let query = 'SELECT ID, 종목명, 종목형태, 매매형태, 매매단가, '+
+    `replace(매매수량, '.00000000', '') AS 매매수량, `+
+    '매매금액, 매매일자 FROM diary WHERE 종목명=?'
+
+    try {
+        let result = await connection.query(query, [name])
+        if (result[0].length < 1) {
+            console.log("nothing")
+        }
+        res.status(202).json(result[0])
+    } catch (err) {
+        console.log(err)
+    } finally {
+        connection.release()
+    }
+}
+
 export const createDiary = async(req, res) => { // async, await
 
     const {종목명, 종목형태, 매매형태, 매매단가, 매매수량, 매매금액, 매매일자} = req.body;
