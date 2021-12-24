1e0cfec9b93d1b9b78af3da1fff214c21ab8cb81
박동혁 <kaje0god@naver.com>
2021-12-24 10:11:26 +0900

    Fix: server posts routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getDiary, createDiary } from '../controllers/posts.js';
+import { getDiary, createDiary, getStock } from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.get('/', getDiary);
 router.post('/', createDiary);
+router.patch('/:name', getStock);
 
 export default router;
