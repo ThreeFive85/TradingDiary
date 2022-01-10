ac54ea310085a5fb49f536ce5513fddb369a9704
박동혁 <kaje0god@naver.com>
2022-01-10 13:12:10 +0900

    Fix: current.js in server routes

--- server/routes/current.js
+++ server/routes/current.js
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { getCurrentStock } from '../controllers/current.js';
+import { getCurrentStock, updateCurrent } from '../controllers/current.js';
 
 const router = express.Router();
 
 router.get('/', getCurrentStock);
+router.get('/:name', updateCurrent);
 export default router;
