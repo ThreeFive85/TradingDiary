782ca2b9deb7d271cb708602bb608331a4895e3c
박동혁 <kaje0god@naver.com>
2022-01-13 13:16:01 +0900

    Fix: index.js in client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,3 +9,4 @@ export const createDiary = (newPost) => axios.post(url, newPost);
 export const fetchCurrents = () => axios.get(url2);
 export const fetchComplete = () => axios.get(url3);
 export const getStock = (name) => axios.patch(`http://localhost:4000/diary/${name}`, name);
+export const updateCurrent = (name) => axios.get(`http://localhost:4000/current/${name}`, name);
