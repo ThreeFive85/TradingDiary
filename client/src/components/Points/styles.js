ff0ee0faa31ac62162f50215ceb88f0ce0eb4f81
박동혁 <kaje0god@naver.com>
2022-01-26 10:54:39 +0900

    Feat: Points component styles

---
+++ client/src/components/Points/styles.js
@@ -0,0 +1,27 @@
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+export const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+export const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+export const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
