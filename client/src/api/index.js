30e6e5af05e86825589fe7f37d4a340b51ca7fe9
박동혁 <kaje0god@naver.com>
2022-01-30 14:36:40 +0900

    Fix: index.js in client api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const url = 'http://localhost:4000/diary';
 const url2 = 'http://localhost:4000/current';
 const url3 = 'http://localhost:4000/complete';
+const url4 = 'http://localhost:4000/points'
 
 export const fetchPosts = () => axios.get(url);
 export const createDiary = (newPost) => axios.post(url, newPost);
@@ -10,3 +11,4 @@ export const fetchCurrents = () => axios.get(url2);
 export const fetchComplete = () => axios.get(url3);
 export const getStock = (name) => axios.patch(`http://localhost:4000/diary/${name}`, name);
 export const updateCurrent = (name) => axios.get(`http://localhost:4000/current/${name}`, name);
+export const getPoints = () => axios.get(url4);
