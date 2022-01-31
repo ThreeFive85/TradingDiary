a84bb56b81792ee69527366e1c1e7f8686ef4458
박동혁 <kaje0god@naver.com>
2022-01-31 10:04:44 +0900

    Fix: index.js in client

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,5 +3,6 @@ import { combineReducers } from 'redux';
 import posts from './posts';
 import current from './current';
 import complete from './complete';
+import points from './points';
 
-export default combineReducers({ posts, current, complete });
+export default combineReducers({ posts, current, complete, points });
