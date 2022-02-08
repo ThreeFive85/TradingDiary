1db397d6206b586034266f3296e6d7b108affd1e
박동혁 <kaje0god@naver.com>
2022-02-08 17:54:00 +0900

    Fix: HeaderBar.js

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -1,33 +1,35 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import {Toolbar, Button} from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 import useStyles from './styles';
 
 const HeaderBar = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-    return (
-      <AppBar className={classes.root} position="static">
-        <Toolbar className={classes.toolbar}>
-          <div className={classes.buttons}>
-          <Button component={Link} to="/" variant="contained" color="secondary">
-            main
-          </Button>
-          <Button component={Link} to="/diary" variant="contained" color="secondary">
-            history
-          </Button>
-          <Button component={Link} to="/complete" variant="contained" color="secondary">
-            rate
-          </Button>
-          <Button component={Link} to="/points" variant="contained" color="secondary">
-            stock points
-          </Button>
-          </div>
-        </Toolbar>
-      </AppBar>
-    )
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="secondary"
+        centered
+      >
+        <Tab label="main" component={Link} to="/"/>
+        <Tab label="history" component={Link} to="/diary"/>
+        <Tab label="rate" component={Link} to="/complete"/>
+        <Tab label="points" component={Link} to="/points"/>
+      </Tabs>
+    </Paper>
+  )
 }
 
 export default HeaderBar
