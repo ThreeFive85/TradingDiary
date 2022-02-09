b159ab0803fec62f3817ef00a9db36b36b2e8f1d
박동혁 <kaje0god@naver.com>
2022-02-09 16:43:38 +0900

    Fix: HeaderBar styles

--- client/src/components/HeaderBar/styles.js
+++ client/src/components/HeaderBar/styles.js
@@ -1,25 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-    root: {
-      borderRadius: 15,
-      margin: '30px 0',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: '10px 150px',
-      },
-      toolbar: {
-        display: 'flex',
-        justifyContent: 'flex-end',
-        width: '690px',
-      },
-      buttons: {
-        '& > *': {
-          margin: theme.spacing(5),
-          alignItems: 'center',
-        },
-      },
-  }));
-  
+  root: {
+    flexGrow: 1,
+    margin: '30px 0',
+    },
+}));
