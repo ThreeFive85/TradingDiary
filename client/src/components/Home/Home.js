4e4a98339b8ef31a76164bd845d12837d1412fb1
박동혁 <kaje0god@naver.com>
2022-02-12 15:05:44 +0900

    Fix: Home component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import Record from '../Record/Record';
 import Holding from '../Holding/Holding';
 
 import {getCurrent} from '../../actions/current';
-// import useStyles from '../../styles';
 
 function Home() {
     const [currentId, setCurrentId] = useState(null);
-    // const classes = useStyles();
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -18,18 +15,12 @@ function Home() {
     }, [dispatch]);
 
     return (
-        <Grow in>
-            <Container>
-                {/* <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}> */}
-                    <Grid >
-                        <Holding setCurrentId={setCurrentId}/>
-                    </Grid>
-                    <Grid >
-                        <Record currentId={currentId} setCurrentId={setCurrentId}/>
-                    </Grid>
-                {/* </Grid> */}
-            </Container>
-        </Grow>
+        <><div>
+            <Holding setCurrentId={setCurrentId}/>
+        </div>
+        <div>
+            <Record currentId={currentId} setCurrentId={setCurrentId}/>
+        </div></>
     )
 }
 
