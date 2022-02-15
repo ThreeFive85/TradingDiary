0d4c8bf649872670ef4493878a42b441e576d2c1
박동혁 <kaje0god@naver.com>
2022-02-15 12:31:39 +0900

    Fix: Holding component styles

--- client/src/components/Holding/styles.js
+++ client/src/components/Holding/styles.js
@@ -1,17 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  // root: {
-    
-  //     marginBottom: 30
-
-  // },
     table: {
         minWidth: 700,
       },
       heading: {
         fontSize: theme.typography.pxToRem(35),
         fontWeight: theme.typography.fontWeightRegular,
+        marginBottom: 10
       },
       choice: {
         margin: theme.spacing(0.5),
