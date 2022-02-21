5689675e0c01ad42ed1553bfb5fc858884e2c31b
박동혁 <kaje0god@naver.com>
2022-02-21 15:40:42 +0900

    Fix: current reducers

--- client/src/reducers/current.js
+++ client/src/reducers/current.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (current = [], action) => {
     switch (action.type) {
-        // case DELETE:
-        //     return posts.filter((post) => post._id !== action.payload);
-        // case "UPDATE_CUR":
-        //     return current.map((post) => post);
+        case "DELETE":
+            return current.filter((post) => post.NAME !== action.payload);
+        case "UPDATE_CUR":
+            // console.log('current : ', current)
+            return current.map((post) => post.NAME === action.payload.NAME ? action.payload : post);
         case "FETCH_ALL_CUR":
             return action.payload;
-        // case "CREATE_CUR":
-        //     return [...current, action.payload];
+        case "CREATE_CUR":
+            return [...current, action.payload];
         default:
             return current;
     }
