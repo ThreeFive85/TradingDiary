f61bc702a8b884f2884cbee5862948b8b44d0438
박동혁 <kaje0god@naver.com>
2022-02-22 18:25:00 +0900

    Fix: current.js in server

--- server/controllers/current.js
+++ server/controllers/current.js
@@ -13,34 +13,11 @@ export const getCurrentStock = async(req, res) => { // async, await
 
     try {
         let result1 = await connection.query(query)
-        let result2 = await connection.query("SELECT * FROM currentStock WHERE CURRENT_COUNT = 0")
-        // console.log(result2[0])
+        
         if (result1[0].length < 1) {
             console.log('nothing')
         }
         
-        if (result2[0].length > 0) {
-            for(let i = 0; i < result2[0].length; i++){
-                let name = result2[0][i].NAME;
-                let buy_money = result2[0][i].BUY_MONEY;
-                let sell_money = result2[0][i].SELL_MONEY;
-                let first_day = result2[0][i].FIRST_DAY;
-                let final_day = result2[0][i].CURRENT_DAY;
-    
-                let insertData = {
-                    name: name,
-                    buy_money: buy_money,
-                    sell_money: sell_money,
-                    first_day: first_day,
-                    final_day: final_day
-                }
-    
-                const [data, fields] = await connection.query('insert into completeStock set ?', insertData);
-                console.log(data)
-                const [data1, fields1] = await connection.query('delete from currentStock where NAME = ?',[name]);
-                console.log(data1)
-            }
-        }
         res.status(202).json(result1[0])
     } catch (err) {
         console.log(err)
@@ -50,19 +27,96 @@ export const getCurrentStock = async(req, res) => { // async, await
 }
 
 export const updateCurrent = async(req, res) => { // async, await
-
-    const {name} = req.params;
-    // console.log(name)
+    const {종목명, 종목형태, 매매형태, 매매단가, 매매수량, 매매금액, 매매일자} = req.body;
 
     const connection = await pool.getConnection();
-    let query = 'SELECT NAME, VALUE FROM currentStock WHERE NAME=?'
+    
+    const plusQuery = 'update currentStock set current_count = current_count + ?,'+
+                        'CURRENT_MONEY = CURRENT_MONEY + ?, BUY_MONEY = BUY_MONEY + ?, CURRENT_DAY=? where NAME = '
+    const minusQuery = 'update currentStock set current_count = current_count - ?, '+
+                        'CURRENT_MONEY = CURRENT_MONEY - ?, SELL_MONEY = SELL_MONEY + ?, CURRENT_DAY=? where NAME = '
 
     try {
-        let result = await connection.query(query, [name])
-        if (result[0].length < 1) {
-            console.log("nothing")
+        if(매매형태 === '매수'){
+            const current_count = 매매수량
+            const current_money = 매매금액
+            const current_day = 매매일자
+            const [plusData, fields2] = await connection.query(plusQuery+mysql.escape(req.body.종목명), [current_count, current_money, current_money, current_day])
+            let query = 'SELECT NAME, VALUE,'+
+            `replace(CURRENT_COUNT, '.00000000', '') AS CURRENT_COUNT, `+
+            'CURRENT_MONEY, BUY_MONEY, SELL_MONEY, FIRST_DAY, CURRENT_DAY '+
+            'FROM currentStock WHERE CURRENT_COUNT > 0 and NAME=?'
+        
+            const result = await connection.query(query, [종목명])
+            console.log(result[0][0])
+            res.status(202).json(result[0][0])
+        } else {
+            const current_count = 매매수량
+            const current_money = 매매금액
+            const current_day = 매매일자
+            const [minusData, fields2] = await connection.query(minusQuery+mysql.escape(req.body.종목명), [current_count, current_money, current_money, current_day])
+            let query = 'SELECT NAME, VALUE,'+
+                    `replace(CURRENT_COUNT, '.00000000', '') AS CURRENT_COUNT, `+
+                    'CURRENT_MONEY, BUY_MONEY, SELL_MONEY, FIRST_DAY, CURRENT_DAY '+
+                    'FROM currentStock WHERE NAME=?'
+                
+            const result = await connection.query(query, [종목명])
+            // console.log(result[0][0])
+            if(result[0][0].CURRENT_COUNT === '0'){
+                let insertData = {
+                    name: result[0][0].NAME,
+                    buy_money: result[0][0].BUY_MONEY,
+                    sell_money: result[0][0].SELL_MONEY,
+                    first_day: result[0][0].FIRST_DAY,
+                    final_day: result[0][0].CURRENT_DAY
+                }
+    
+                const [data, fields] = await connection.query('insert into completeStock set ?', insertData);
+                console.log(data)
+                const [data1, fields1] = await connection.query('delete from currentStock where NAME = ?',[종목명]);
+                console.log(data1)
+                res.json({message: '해당 종목이 completeStock으로 이동'})
+            } else {
+                res.status(202).json(result[0][0]);
+            }
         }
-        res.status(202).json(result[0])
+    } catch (err) {
+        console.log(err)
+    } finally {
+        connection.release()
+    }
+}
+
+export const createCurrent = async(req, res) => { // async, await
+
+    const {종목명, 종목형태, 매매형태, 매매단가, 매매수량, 매매금액, 매매일자} = req.body;
+
+    const insertCurrentData = {
+        NAME: 종목명, 
+        VALUE: 종목형태,
+        CURRENT_COUNT: 매매수량, 
+        CURRENT_MONEY: 매매금액, 
+        BUY_MONEY: 매매금액, 
+        SELL_MONEY: 0,
+        FIRST_DAY: 매매일자
+    }
+
+    const insertCurrentQuery = 'insert into currentStock set ?'
+
+    // console.log(req.body)
+
+    const connection = await pool.getConnection();
+
+    try {
+        const [createData, fields1] = await connection.query(insertCurrentQuery, insertCurrentData);
+        let query = 'SELECT NAME, VALUE,'+
+                    `replace(CURRENT_COUNT, '.00000000', '') AS CURRENT_COUNT, `+
+                    'CURRENT_MONEY, BUY_MONEY, SELL_MONEY, FIRST_DAY, CURRENT_DAY '+
+                    'FROM currentStock WHERE CURRENT_COUNT > 0 and NAME=?'
+                
+        const result = await connection.query(query, [종목명])
+        // console.log(result[0][0])
+        res.status(202).json(result[0][0])
     } catch (err) {
         console.log(err)
     } finally {
