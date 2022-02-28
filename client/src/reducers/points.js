b936d1b1d4e8f5b21ca4ec070af66f7a7882d535
박동혁 <kaje0god@naver.com>
2022-02-28 13:14:54 +0900

    Fix: point.js in client

--- client/src/reducers/points.js
+++ client/src/reducers/points.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (current = [], action) => {
+export default (points = [], action) => {
     switch (action.type) {
         case "FETCH_ALL_POI":
             return action.payload;
-        
+        case "UPDATE_POINT":
+            return points.map((post) => post.day === action.payload.day ? action.payload : post);
         default:
-            return current;
+            return points;
     }
 }
